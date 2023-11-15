701d620a608ba0965642c08934c3118e0c65395d
Hordur Thor Jonsson <htjonsson@gmail.com>
2023-11-15 01:14:58 +0000

    Init Project

---
+++ frontend/antd-crud/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Layout, version } from 'antd';
+import SimpleTable from './SimpleTable';
+const { Header, Content } = Layout;
+
+const App = () => {
+
+    return (
+        <Layout>
+            <Header
+                style={{
+                    color: 'white',
+                    fontWeight: 'bold',
+                    textAlign: 'center'
+                }}
+            >
+                Ant Design Version: {version}
+            </Header>
+            <Content
+                style={{
+                    padding: '24px 24px',
+                }}
+            >
+                <SimpleTable></SimpleTable>
+            </Content>
+        </Layout>
+    );
+}
+
+export default App;
